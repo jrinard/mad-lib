e23637c710435428fcbfd4a4a5097b1046d9efd7
Jesse McKay and Nigel Burnett <jesse@>
2016-10-19 10:02:26 -0700

    added capitalization function using Javascript so that all of our madlib inputs are capitalized.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   $("#blank form").submit(function(event) {
-    var personInput = $("input#person").val();
-    var placeInput = $("input#place").val();
-    var thingInput = $("input#thing").val();
-    var colorInput = $("input#color").val();
+    var personInput = $("input#person").val().toUpperCase();
+    var placeInput = $("input#place").val().toUpperCase();
+    var thingInput = $("input#thing").val().toUpperCase();
+    var colorInput = $("input#color").val().toUpperCase();
+
 
     $(".person").text(personInput);
     $(".place").text(placeInput);
     $(".thing").text(thingInput);
     $(".color").text(colorInput);
 
-    $("#story").show();
+
+    $("#story").fadeIn(500);
+
 
     event.preventDefault();
 
