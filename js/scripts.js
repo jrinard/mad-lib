743dda0bf2075321684b8dfae7a5d2444a10ec31
Joshua Rinard and Nigel Burnett <nigel@>
2016-10-24 11:16:42 -0700

    removes repetition and adds a forEach loop to handle userinput

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-  $("#blank form").submit(function(event) {
-    var personInput = $("input#person").val().toUpperCase();
-    var placeInput = $("input#place").val().toUpperCase();
-    var thingInput = $("input#thing").val().toUpperCase();
-    var colorInput = $("input#color").val().toUpperCase();
+  $("#blanks form").submit(function(event) {
+    var blanks = ["person", "place", "thing", "color"];
 
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    $(".person").text(personInput);
-    $(".place").text(placeInput);
-    $(".thing").text(thingInput);
-    $(".color").text(colorInput);
-
+// old version before loop
+    // var personInput = $("input#person").val().toUpperCase();
+    // var placeInput = $("input#place").val().toUpperCase();
+    // var thingInput = $("input#thing").val().toUpperCase();
+    // var colorInput = $("input#color").val().toUpperCase();
+    //
+    //
+    // $(".person").text(personInput);
+    // $(".place").text(placeInput);
+    // $(".thing").text(thingInput);
+    // $(".color").text(colorInput);
+    //
 
     $("#story").fadeIn(500);
 
